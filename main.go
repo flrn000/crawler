94c75f50d6abb0a8411506f9af7b07235a8a93bb
Iacob Florin <iacob.florin000@gmail.com>
2024-08-28 02:53:34 +0300

    read commandline arguments

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	cmdArgs := os.Args[1:]
+
+	if len(cmdArgs) < 1 {
+		fmt.Println("no website provided")
+		os.Exit(1)
+	} else if len(cmdArgs) > 1 {
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	} else {
+		fmt.Printf("starting crawl\n%v\n", cmdArgs[0])
+	}
 }
